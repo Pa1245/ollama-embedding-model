47c7eb29dbbc29c44235d908a114f79ad52c05da
Parag Amrutkar <paragamrutkar1103@gmail.com>
2025-11-17 21:23:36 -0500

    Initial commit

---
+++ example.py
@@ -0,0 +1,34 @@
+import ollama
+import chromadb
+import os
+
+documents = [
+    "Llamas are members of the camelid family meaning they're pretty closely related to vicuñas and camels",
+    "Llamas were first domesticated and used as pack animals 4,000 to 5,000 years ago in the Peruvian highlands",
+    "Llamas can grow as much as 6 feet tall though the average llama between 5 feet 6 inches and 5 feet 9 inches tall",
+    "Llamas weigh between 280 and 450 pounds and can carry 25 to 30 percent of their body weight",
+    "Llamas are vegetarians and have very efficient digestive systems",
+    "Llamas live to be about 20 years old, though some only live for 15 years and others live to be 30 years old",
+]
+
+client = chromadb.Client()
+collection = client.create_collection(name="docs")
+
+for i, d in enumerate(documents):
+    response = ollama.embed(model="embeddinggemma:300m", input=d)
+    embeddings = response["embeddings"]
+    collection.add(ids=[str(i)], embeddings=embeddings, documents=[d])
+
+prompt = "What animals are llamas related to?"
+
+# generate an embedding for the input and retrieve the most relevant doc
+response = ollama.embed(model="embeddinggemma:300m", input=prompt)
+results = collection.query(query_embeddings=response["embeddings"], n_results=1)
+data = results["documents"][0][0]
+
+output = ollama.generate(
+  model="gemma3:4b",
+  prompt=f"Using this data: {data}. Respond to this prompt: {prompt}"
+)
+
+print(output['response'])
